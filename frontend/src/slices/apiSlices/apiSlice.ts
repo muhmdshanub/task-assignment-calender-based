cae6bb57d04c712ae9ee247529535c008c4b06e2
Muhammad shanoob a k <60563775+muhmdshanub@users.noreply.github.com>
2024-10-22 11:59:14 +0530

    Update apiSlice.ts

--- frontend/src/slices/apiSlices/apiSlice.ts
+++ frontend/src/slices/apiSlices/apiSlice.ts
@@ -5,6 +5,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_BASE_API_URL , // Ensure this is defined in your .env file
   credentials: 'include', // Include credentials with every request, if needed
+  optionsSuccessStatus: 200,
 });
 
 export const apiSlice = createApi({
